59c8cd47a3879495c9e200d71a3908c1ddab361b
alfian10 <alfian.suhendar@gmail.com>
2024-05-31 15:23:59 +0700

    new

--- channel_test.go
+++ channel_test.go
@@ -20,5 +20,21 @@ func TestCreateChannel(t *testing.T) {
 	fmt.Println(data)
 
 	time.Sleep(5 * time.Second)
+}
 
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Eko Kurniawan Khannedy"
+}
+
+func TestChannelAsParameter(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go GiveMeResponse(channel)
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
 }
